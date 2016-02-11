00f9d9503c751dc3bd8d4c82494f08a5b0aa7cce
Louise Wilson <weesie.wilson@Louises-MacBook-Pro.local>
2016-02-11 00:19:09 -0500

    finished first javascript assignment

---
+++ javascript.js
@@ -0,0 +1,153 @@
+var answer = prompt ("Choose a Superpower: Super Strength, Invisiblity, Fly.");
+  if (answer === "Super Strength"){
+    console.log("great choice");
+    answer = prompt("Choose a Cuisine: Italian, Mexican, Seafood");
+      if (answer === "Italian"){
+        console.log("awesome");
+        answer = prompt("Choose a Color: Blue, Orange, Red");
+          if (answer === "Blue"){
+            console.log("cool");
+            alert("You are Darla from Finding Nemo");
+          }
+          else if (answer === "Orange") {
+            alert("You are Lord Farquad from Shrek");
+
+          }
+          else {
+            alert("You are Scarlet Overkill from Despicable Me");
+          }
+      }
+      else if(answer === "Mexican"){
+        console.log("awesome");
+        answer = prompt("Choose a Color: Blue, Orange, Red");
+          if (answer === "Blue"){
+            console.log("cool");
+            alert("You are Syndrome from the Incredibles");
+          }
+          else if (answer === "Orange") {
+            alert("You are Vector from Despicable Me");
+
+          }
+          else {
+            alert("You are El Macho from Despicable Me");
+          }
+      }
+      else{
+        console.log("awesome");
+        answer = prompt("Choose a Color: Blue, Orange, Red");
+          if (answer === "Blue"){
+            console.log("cool");
+            alert("You are Bruce from Finding Nemo");
+          }
+          else if (answer === "Orange") {
+            alert("You are Randall Boggs from Monsters Inc.");
+
+          }
+          else {
+            alert("You are Henry Watermoose from Monsters Inc.");
+          }
+
+      }
+  }
+  else if(answer == "Invisiblity"){
+    console.log("great choice");
+    answer = prompt("Choose a Dessert: Cake, Ice Cream, Brownie");
+      if (answer === "Cake"){
+        console.log("awesome");
+        answer = prompt("Choose a Drink: Coffee, Soda, Tea");
+          if (answer === "Coffee"){
+            console.log("cool");
+            alert("You are Dory from Finding Nemo");
+          }
+          else if (answer === "Soda") {
+            alert("You are Gru from Despicable Me");
+
+          }
+          else {
+            alert("You are Woody from Toy Story");
+          }
+      }
+      else if(answer === "Ice Cream"){
+        console.log("awesome");
+        answer = prompt("Choose a Drink: Coffee, Soda, Tea");
+          if (answer === "Coffee"){
+            console.log("cool");
+            alert("You are Buzz Lightyear from Toy Story");
+          }
+          else if (answer === "Soda") {
+            alert("You are Sully from Monsters Inc.");
+
+          }
+          else {
+            alert("You are Nemo from Finding Nemo");
+          }
+      }
+      else{
+        console.log("awesome");
+        answer = prompt("Choose a Drink: Coffee, Soda, Tea");
+          if (answer === "Coffee"){
+            console.log("cool");
+            alert("You are Mike Wazowski from Monsters Inc.");
+          }
+          else if (answer === "Soda") {
+            alert("You are Crush from Finding Nemo");
+
+          }
+          else {
+            alert("You are Mr. Incredible from The Incredibles");
+          }
+
+      }
+
+  }
+  else {
+    console.log("great choice");
+    answer = prompt("Choose a Sport: Basketball, Tennis, Football");
+      if (answer === "Basketball"){
+        console.log("awesome");
+        answer = prompt("Choose a Car: Jeep, Ford, Toyota");
+          if (answer === "Jeep"){
+            console.log("cool");
+            alert("You are Puss in Boots from Shrek");
+          }
+          else if (answer === "Ford") {
+            alert("You are Shrek from Shrek");
+
+          }
+          else {
+            alert("You are Donkey from Shrek");
+          }
+      }
+      else if(answer === "Tennis"){
+        console.log("awesome");
+        answer = prompt("Choose a Car: Jeep, Ford, Toyota");
+          if (answer === "Jeep"){
+            console.log("cool");
+            alert("You are Russell from Up");
+          }
+          else if (answer === "Ford") {
+            alert("You are Carl from Up");
+
+          }
+          else {
+            alert("You are Dr. Nefario from Despicable Me");
+          }
+      }
+      else{
+        console.log("awesome");
+        answer = prompt("Choose a Car: Jeep, Ford, Toyota");
+          if (answer === "Jeep"){
+            console.log("cool");
+            alert("You are Boo from Monsters Inc.");
+          }
+          else if (answer === "Ford") {
+            alert("You are Elastigirl from The Incredibles");
+
+          }
+          else {
+            alert("You are a Minion from Despicable Me");
+          }
+
+      }
+
+  }
